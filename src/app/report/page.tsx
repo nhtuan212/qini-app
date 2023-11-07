ee2634dbe7eaa79e6c012db426bf753e2dc5e066
Hoang Tuan <nhtuan212@gmail.com>
2023-11-07 20:46:26 +0700

    #add database

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+// import React from "react";
 
-//** Postgres */
-import { sql } from "@vercel/postgres";
+// //** Postgres */
+// import { sql } from "@vercel/postgres";
 
-export default async function ReportPage({
-    params,
-}: {
-    params: { user: string };
-}): Promise<JSX.Element> {
-    const { rows } =
-        await sql`SELECT * from CARTS where user_id=${params.user}`;
+// export default async function ReportPage({
+//     params,
+// }: {
+//     params: { user: string };
+// }): Promise<JSX.Element> {
+//     const { rows } =
+//         await sql`SELECT * from CARTS where user_id=${params.user}`;
 
-    return (
-        <div>
-            {rows.map(row => (
-                <div key={row.id}>
-                    {row.id} - {row.quantity}
-                </div>
-            ))}
-        </div>
-    );
-}
+//     return (
+//         <div>
+//             {rows.map(row => (
+//                 <div key={row.id}>
+//                     {row.id} - {row.quantity}
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
